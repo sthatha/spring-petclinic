857821458dc81e98afcfdf2aaa5c9db289cc13ec
sthatha <thatha.srinivasulu@gmail.com>
2025-04-06 16:47:32 +0530

    Organized the code

--- src/main/java/org/springframework/samples/petclinic/pet/PetTypeFormatter.java
+++ src/main/java/org/springframework/samples/petclinic/pet/PetTypeFormatter.java
@@ -13,9 +13,10 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package org.springframework.samples.petclinic.owner;
+package org.springframework.samples.petclinic.pet;
 
 import org.springframework.format.Formatter;
+import org.springframework.samples.petclinic.owner.OwnerRepository;
 import org.springframework.stereotype.Component;
 
 import java.text.ParseException;
--- src/test/java/org/springframework/samples/petclinic/owner/PetTypeFormatterTests.java
+++ src/test/java/org/springframework/samples/petclinic/owner/PetTypeFormatterTests.java
@@ -32,6 +32,8 @@ import org.junit.jupiter.api.condition.DisabledInNativeImage;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.Mock;
 import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.samples.petclinic.pet.PetType;
+import org.springframework.samples.petclinic.pet.PetTypeFormatter;
 
 /**
  * Test class for {@link PetTypeFormatter}
